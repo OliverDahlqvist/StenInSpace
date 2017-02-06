7ac1ef6de0798d7b3e82d7a1631bf62e102fa9fb
OliverDahlqvist <oliverdahlqvist97@gmail.com>
2017-02-06 15:38:04 +0100

    Initial Commit

---
+++ Mine/Assets/Scripts/Mine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour {
+    public GameObject MineStone;
+    //public GameObject Camera;
+    MineStone mineScript;
+    CameraShake cameraShake;
+    public GameObject PS_Sparks;
+    Animator animator;
+    public AudioClip[] hitSound;
+    AudioSource audio;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        mineScript = MineStone.GetComponent<MineStone>();
+        cameraShake = MineStone.GetComponent<CameraShake>();
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("Mining", true);
+        }
+        else
+        {
+            animator.SetBool("Mining", false);
+        }
+	}
+    void Spark()
+    {
+        if (mineScript.rayHit != null)
+        {
+            audio.clip = hitSound[Random.Range(0, hitSound.Length)];
+            audio.Play();
+            mineScript.addStones = true;
+            Instantiate(PS_Sparks, mineScript.hit.point, Quaternion.LookRotation(mineScript.hit.normal));
+            cameraShake.shakeDuration += 0.2f;
+        }
+        else
+        {
+            mineScript.addStones = false;
+        }
+    }
+}
---
+++ Mine/Assets/Scripts/Stone.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stone : MonoBehaviour {
+    public float amountStones;
+    float perc;
+    GameObject r_01;
+    GameObject r_02;
+    GameObject r_03;
+    GameObject r_04;
+    GameObject currentObj;
+
+    void Start () {
+        amountStones = Random.Range(100, 200);
+        perc = amountStones;
+        r_01 = transform.Find("rock_01").gameObject;
+        r_02 = transform.Find("rock_02").gameObject;
+        r_03 = transform.Find("rock_03").gameObject;
+        r_04 = transform.Find("rock_04").gameObject;
+        r_02.SetActive(false);
+        r_03.SetActive(false);
+        r_04.SetActive(false);
+        currentObj = r_01;
+	}
+	
+    public void UpdateStone()
+    {
+        //currentObj.transform.localScale -= new Vector3(0.01F / perc, 0.01F / perc, 0.01F);
+        if(amountStones <= perc * 0.8 && amountStones > perc * 0.6)
+        {
+            r_01.SetActive(false);
+            r_02.SetActive(true);
+            currentObj = r_02;
+        }
+        else if(amountStones <= perc * 0.6 && amountStones > perc * 0.4)
+        {
+            currentObj.SetActive(false);
+            r_03.SetActive(true);
+            currentObj = r_03;
+        }
+        else if (amountStones <= perc * 0.4)
+        {
+            currentObj.SetActive(false);
+            r_04.SetActive(true);
+            currentObj = r_04;
+        }
+
+        if (amountStones <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
